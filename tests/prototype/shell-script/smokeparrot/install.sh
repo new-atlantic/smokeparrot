1dd0463f697e0a735a84aee3d054afdfc5a863e1
Torsti Schulz <torsti.schulz@iki.fi>
2011-06-26 15:27:03 +0300

    Unfinished script to automate transfer of Smokeparrot to a target device.

---
+++ tests/prototype/shell-script/smokeparrot/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Script to create the smokeparrot directory structure on the a remote device.
+# TODO: creating a smokeparrot-user, transfering ssh-keys and more.
+
+TARGET_DEVICE="$1"
+
+/usr/bin/ssh root@$TARGET_DEVICE 'mkdir /smokeparrot'
+/usr/bin/ssh root@$TARGET_DEVICE 'mkdir /smokeparrot/messages /smokeparrot/queue'
+/usr/bin/scp -r cgi/ "root@$TARGET_DEVICE:/smokeparrot/"
+/usr/bin/scp -r lib/ "root@$TARGET_DEVICE:/smokeparrot/"
+/usr/bin/scp -r html/ "root@$TARGET_DEVICE:/smokeparrot/"
+/usr/bin/scp settings "root@$TARGET_DEVICE:/smokeparrot/"
+
+/usr/bin/ssh root@$TARGET_DEVICE 'ln -s /smokeparrot/cgi/smokeparrot.cgi /www/cgi-bin/smokeparrot.cgi'
