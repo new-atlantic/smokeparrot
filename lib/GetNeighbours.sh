aa322a65df1beed3e609295f376c84852388c61b
Torsti Schulz <torsti.schulz@iki.fi>
2011-07-18 12:43:07 +0300

    Fix neighbour detection to only get next-hops.

--- lib/GetNeighbours.sh
+++ lib/GetNeighbours.sh
@@ -2,7 +2,7 @@
 # This script gets a list of current neighbours.
 
 SMOKEPARROT_PATH=/home/smokeparrot
-NEIGHBOUR_MAC_LIST="$(cat /sys/kernel/debug/batman_adv/bat0/originators | /bin/sed -n '3,$p' | /usr/bin/awk '{ print $1 }')"
+NEIGHBOUR_MAC_LIST="$(/bin/cat /sys/kernel/debug/batman_adv/bat0/originators | /bin/sed -n '3,$p' | /usr/bin/awk '{$1=$2=""; print $0}' | grep -E -o '[[:xdigit:]]{2}(:[[:xdigit:]]{2}){5}' | /usr/bin/sort | /usr/bin/uniq)"
 
 ADDRESS_LIST=$(cat $SMOKEPARROT_PATH/neighbour.list | /usr/bin/awk '{ print $2 }')
 
