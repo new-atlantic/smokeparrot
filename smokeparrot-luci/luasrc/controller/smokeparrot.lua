a0d82f987510b54b47fe18f3696e1c176747dfa2
Torsti Schulz <torsti.schulz@iki.fi>
2011-11-28 00:11:11 +0200

    Add function calls for POST-requests and sqlite storage
    * Sqlite via luasql
    * Does not check if database has been changed between reading
      messages and operating on messages
    * Database schema temporary

--- smokeparrot-luci/luasrc/controller/smokeparrot.lua
+++ smokeparrot-luci/luasrc/controller/smokeparrot.lua
@@ -21,6 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module("luci.controller.smokeparrot", package.seeall)
 
+require "luasql.sqlite3"
+local db_file = "/tmp/smokeparrot_test.sqlite3"
+
 function index()
    entry({"apps", "smokeparrot"}, alias("apps", "smokeparrot", "stream"), "Smokeparrot", 90).dependent=false
    entry({"apps", "smokeparrot", "stream"}, call("display_messages"), "Stream", 10).dependent=false
@@ -29,40 +32,115 @@ function index()
    entry({"apps", "smokeparrot", "help"}, template("smokeparrot/help"), "Help", 40).dependent=false
 
    node("apps", "smokeparrot", "stream", "post").target = call("post_message")
+   node("apps", "smokeparrot", "stream", "star").target = call("star_message")
+   node("apps", "smokeparrot", "stream", "share").target = call("share_message")
+   node("apps", "smokeparrot", "stream", "hide").target = call("hide_message")
 end
 
 function post_message()
-   local value = luci.http.formvalue("sp_message-body") or luci.http.formvalue("sp_star-message") or luci.http.formvalue("sp_share-message") or luci.http.formvalue("sp_hide-message")
+   local env = assert (luasql.sqlite3())
+   local conn = assert (env:connect(db_file))
+   local id = luci.http.formvalue("sp_message-body")
+   local res = assert (conn:execute(string.format([[
+	INSERT INTO messages VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')]],
+                math.random(1, 100000), "", os.time(),
+		luci.http.formvalue("sp_message-body"), 0, 1, 1, 0 )))
+   conn:close()
+   env:close()
+   luci.http.redirect(luci.dispatcher.build_url("apps", "smokeparrot"))
+end
 
-   if luci.http.formvalue("sp_message-body") then
-      do_nothing = nil
-   elseif luci.http.formvalue("sp_star-message") then
-      do_nothing = nil
-   elseif luci.http.formvalue("sp_share-message") then
-      do_nothing = nil
-   elseif luci.http.formvalue("sp_hide-message") then
-      do_nothing = nil
+function star_message()
+   local env = assert (luasql.sqlite3())
+   local conn = assert (env:connect(db_file))
+
+   local id = luci.http.formvalue("sp_star-message")
+   local state = luci.http.formvalue("sp_star-state")
+
+   if state == "0" then
+      state = 1
+   else
+      state = 0
    end
+
+   local res = assert (conn:execute(string.format([[
+	UPDATE messages SET "starred" = '%s' WHERE "id" = '%s';]], state, id )))
+
+   conn:close()
+   env:close()
+
+   luci.http.redirect(luci.dispatcher.build_url("apps", "smokeparrot"))
+end
+
+function share_message()
+   local env = assert (luasql.sqlite3())
+   local conn = assert (env:connect(db_file))
+
+   local id = luci.http.formvalue("sp_share-message")
+   local state = luci.http.formvalue("sp_share-state")
+
+   if state == "0" then
+      state = 1
+   else
+      state = 0
+   end
+
+   local res = assert (conn:execute(string.format([[
+	UPDATE messages SET "shared" = '%s' WHERE "id" = '%s';]], state, id )))
+
+   conn:close()
+   env:close()
+
+   luci.http.redirect(luci.dispatcher.build_url("apps", "smokeparrot"))
+end
+
+function hide_message()
+   local env = assert (luasql.sqlite3())
+   local conn = assert (env:connect(db_file))
+
+   local id = luci.http.formvalue("sp_hide-message")
+   local state = luci.http.formvalue("sp_hide-state")
+
+   if state == "0" then
+      state = 1
+   else
+      state = 0
+   end
+   local res = assert (conn:execute(string.format([[
+	UPDATE messages SET "hidden" = '%s' WHERE "id" = '%s';]], state, id )))
+
+   conn:close()
+   env:close()
 
    luci.http.redirect(luci.dispatcher.build_url("apps", "smokeparrot"))
 end
 
 function display_messages()
-   local dummy_message = {
-      id = "message_id",
-      name = "message_sender_name",
-      node = "originating_node",
-      text = "message_text",
-      time = "1",
-      starred = false,
-      own = false,
-      shared = true,
-      visible = true,
-   }
+   local env = assert (luasql.sqlite3())
+   local conn = assert (env:connect(db_file))
+   local cur = assert (conn:execute"SELECT id, name, time, text, own, starred, shared, hidden FROM messages")
+   local row = cur:fetch ({}, "a")
 
-   local messages = { dummy_message, }
+   local messages = {}
+
+   while row do
+      table.insert (messages, {
+		       id = row.id,
+		       name = row.name,
+		       time = row.time,
+		       node = "",
+		       text = row.text,
+		       starred = row.starred,
+		       own = row.own,
+		       shared = row.shared,
+		       hidden = row.hidden})
+      row = cur:fetch (row, "a")
+   end
+
+   cur:close()
+   conn:close()
+   env:close()
 
    table.sort(messages, function (a,b) return (a.time > b.time) end)
-
    luci.template.render("smokeparrot/stream", { messages = messages })
 end
