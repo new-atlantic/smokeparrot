9d9fd845002c18bb7d9f62a382e75d99d6f8a066
Torsti Schulz <torsti.schulz@iki.fi>
2012-02-25 15:02:49 +0200

    Use extern with function prototypes for clarity

--- smokeparrot-daemon/src/SP_http_response.h
+++ smokeparrot-daemon/src/SP_http_response.h
@@ -36,11 +36,11 @@
  * SP_request_callback parses the request and returns the appropriate response.
  */
 
-int SP_request_callback (void *cls, struct MHD_Connection *connection,
-                         const char *url,
-                         const char *method, const char *version,
-                         const char *upload_data,
-                         size_t *upload_data_size, void **con_cls);
+extern int SP_request_callback (void *cls, struct MHD_Connection *connection,
+                                const char *url,
+                                const char *method, const char *version,
+                                const char *upload_data,
+                                size_t *upload_data_size, void **con_cls);
 
 #endif
 
